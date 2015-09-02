d7f9d6ad5c4bb0ffb9577779c2100a214652ca32
Federico Voges <federico@puppetlabs.com>
2015-09-02 12:10:47 +0100

    Update exec command logic for 2015.x and remove trailing spaces

--- puppet/modules/razor_dnsmasq/manifests/init.pp
+++ puppet/modules/razor_dnsmasq/manifests/init.pp
@@ -12,25 +12,25 @@ class razor_dnsmasq {
     require => Package['dnsmasq'],
   }
 
-  file { "${dnsmasq_config_dir}" : 
+  file { "${dnsmasq_config_dir}" :
     ensure => directory,
   }
 
-  file { "${dnsmasq_config_dir}/hosts" : 
+  file { "${dnsmasq_config_dir}/hosts" :
     ensure => file,
     source => 'puppet:///modules/razor_dnsmasq/dnsmasq.d/hosts',
     require => File["${dnsmasq_config_dir}"],
     notify => Service['dnsmasq'],
   }
- 
-  file { "${dnsmasq_config_dir}/razor" : 
+
+  file { "${dnsmasq_config_dir}/razor" :
     ensure => file,
     source => 'puppet:///modules/razor_dnsmasq/dnsmasq.d/razor',
     require => File["${dnsmasq_config_dir}"],
     notify => Service['dnsmasq'],
   }
 
-  file { "${dnsmasq_config_dir}/virtualbox" : 
+  file { "${dnsmasq_config_dir}/virtualbox" :
     ensure => file,
     source => 'puppet:///modules/razor_dnsmasq/dnsmasq.d/virtualbox',
     require => File["${dnsmasq_config_dir}"],
@@ -53,21 +53,21 @@ class razor_dnsmasq {
     ensure => directory,
     before => Service['dnsmasq']
   }
-  
+
   file { '/var/lib/tftpboot/undionly-20140116.kpxe' :
     ensure => file,
     source => 'puppet:///modules/razor_dnsmasq/undionly-20140116.kpxe',
-  }  
+  }
 
   case versioncmp( $::pe_version, '3.7.99') {
-      '1':  { $ipxe_dl_cmd = "/usr/bin/wget --no-check-certificate 'https://razor-server:8151/api/microkernel/bootstrap?nic_max=1&http_port=8150' -O /var/lib/tftpboot/bootstrap.ipxe" }
-      '-1': { $ipxe_dl_cmd = "/usr/bin/wget --no-check-certificate 'http://razor-server:8080/api/microkernel/bootstrap?nic_max=1' -O /var/lib/tftpboot/bootstrap.ipxe" } 
+      '-1': { $ipxe_dl_cmd = "/usr/bin/wget --no-check-certificate 'http://razor-server:8080/api/microkernel/bootstrap?nic_max=1' -O /var/lib/tftpboot/bootstrap.ipxe" }
+      default:  { $ipxe_dl_cmd = "/usr/bin/wget --no-check-certificate 'https://razor-server:8151/api/microkernel/bootstrap?nic_max=1&http_port=8150' -O /var/lib/tftpboot/bootstrap.ipxe" }
   }
 
   exec { 'get bootstrap.ipxe from razor server' :
-    command => $ipxe_dl_cmd, 
-    tries   => 10,
+    command   => $ipxe_dl_cmd,
+    tries     => 10,
     try_sleep => 30,
-    unless  => '/usr/bin/test -s /var/lib/tftpboot/bootstrap.ipxe',
+    unless    => '/usr/bin/test -s /var/lib/tftpboot/bootstrap.ipxe',
   }
 }
